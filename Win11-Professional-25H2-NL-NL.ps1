f387966099b86368dd8bb0a2a6a3f1d357f473ca
Schuirink <85931455+Schuirink@users.noreply.github.com>
2026-03-02 13:45:16 +0100

    Add files via upload

---
+++ Win11-Professional-25H2-NL-NL.ps1
@@ -0,0 +1,18 @@
+Write-Host  -ForegroundColor Green "Starting AVO OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Green "Start AVO OSDCloud"
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage nl-nl -OSLicense Retail -ZTI
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
